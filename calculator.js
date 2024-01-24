43ac048dc5a1da32c7ef95a0d6dbdca2dc695d50
code_unlimited <137721178+Hossain86@users.noreply.github.com>
2024-01-24 22:50:39 +0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,53 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let operand1 = '';
+
+function appendNumber(number) {
+  currentInput += number;
+  updateDisplay();
+}
+
+function setOperator(op) {
+  operator = op;
+  operand1 = currentInput;
+  currentInput = '';
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentInput = '';
+  operator = '';
+  operand1 = '';
+  updateDisplay();
+}
+
+function calculate() {
+  let result;
+  const num1 = parseFloat(operand1);
+  const num2 = parseFloat(currentInput);
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+  }
+
+  currentInput = result.toString();
+  operator = '';
+  operand1 = '';
+  updateDisplay();
+}
+
+function updateDisplay() {
+  display.value = currentInput;
+}
